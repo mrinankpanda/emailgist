b532e6dfc8866e21e41d4d68f09f990c651e71c4
Mrinank Panda <mrinankpanda@Mrinanks-MacBook-Pro.local>
2025-06-30 14:18:16 -0500

    forgot to setup vite

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import EmailGist from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<EmailGist />);
